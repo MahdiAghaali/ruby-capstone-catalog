cabd393822a71e433d96d4657df323898648f764
MahdiAghaali <mahdi.aghaali@yahoo.com>
2023-01-10 00:59:32 +0330

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,11 @@
+class Main
+  def initialize
+    puts "\n << WELCOME TO the Catalog of things >> \n"
+    show_menu
+  end
+
+
+  def exit
+    puts 'Thank you for using the app, see you later!'
+  end
+end
