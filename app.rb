3b57e3541a6d030aaba2e6982146fa9663ec0d25
MahdiAghaali <mahdi.aghaali@yahoo.com>
2023-01-10 01:05:55 +0330

    Create app.rb to rub the program

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'date'
+require_relative './main'
+
+class App
+  attr_reader :books, :people, :rental
+  
+  def initialize
+    Main.new
+  end
+end
+
+def run_program
+  App.new
+end
+
+run_program
