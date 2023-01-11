bc4ecd466b2e99373751132416527e824c58c148
MahdiAghaali <mahdi.aghaali@yahoo.com>
2023-01-11 21:22:26 +0330

    Create game class

---
+++ Modules/game.rb
@@ -0,0 +1,12 @@
+require_relative './item'
+
+class Game < Item
+  attr_reader :multiplayer, :last_played_at
+
+  def initialize(genre, author, source, label, publish_date, multiplayer, last_played_at)
+    super(genre, author, source, label, publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+end
