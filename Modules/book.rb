95122e452208c2f8a7b29e449cafe0fbd55eb9ba
MahdiAghaali <mahdi.aghaali@yahoo.com>
2023-01-11 20:59:30 +0330

    Create Book class

---
+++ Modules/book.rb
@@ -0,0 +1,13 @@
+require_relative './item'
+
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(genre_in, author_in, source_in, label_in, publish_date_in, publisher_in, cover_state_in)
+    super(genre_in, author_in, source_in, label_in, publish_date_in)
+    @publisher = publisher_in
+    @cover_state = cover_state_in
+    puts genre_in, author_in, source_in, label_in, publish_date_in, publisher_in, cover_state_in
+  end
+
+end
