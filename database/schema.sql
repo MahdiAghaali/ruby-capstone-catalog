206e4107ff2cd0210d3c7b89308b37f9f62973f7
Mahdi Aghaali <Mahdi.aghaali@yahoo.com>
2023-01-12 21:33:26 +0330

    Add schema for label tabel

--- database/schema.sql
+++ database/schema.sql
@@ -9,3 +9,10 @@ CREATE TABLE genre(
   name VARCHAR(255),
   FOREIGN KEY(item_id) REFERENCES items(id)
 );
+
+CREATE TABLE [Label](
+	[id] [int] GENERATED ALWAYS AS INDENTITY PRIMARY KEY,
+	[title] [varchar](50) NULL,
+	[color] [varchar](50) NULL,
+	[items] [nvarchar](MAX) NULL,
+);
