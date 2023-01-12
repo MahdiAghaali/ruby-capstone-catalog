a496fa85a2965518f74bca451a2d14afb4d851c9
alhajideen <salifunurudeen834@gmail.com>
2023-01-12 18:46:20 +0000

    add test cases for Author

--- app.rb
+++ app.rb
@@ -1,16 +1,23 @@
-require 'date'
-require_relative './main'
+require('./Modules/author')
 
-class App
-  attr_reader :books, :people, :rental
+describe 'Testing Author Album functions' do
+  before(:each) do
+    @new_author = Author.new('Igiee','Aria')
+  end
 
-  def initialize
-    Main.new
+  it 'Returns the instance of class' do
+    expect(@new_author).to be_instance_of Author
+  end
+
+  it 'Return the first name' do
+    expect(@new_genre.first_name).to eql 'Igiee'
+  end
+
+  it 'Return the last name' do
+    expect(@new_genre.last_name).to eql 'Aria'
+  end
+
+  it 'Return the id should be in range of 1-1000' do
+    expect(@new_genre.id.length).to eql 36
   end
 end
-
-def run_program
-  App.new
-end
-
-run_program
